6ef49f936a741ae04b14bfcbda964eb090d88122
Mo Mirza <mo.mirza@iwoca.co.uk>
2013-01-25 20:32:43 +0000

    change to add interval compounding

--- cf.py
+++ cf.py
@@ -1,13 +1,13 @@
 from matplotlib import pyplot
 
 def compound_interest(W, r, n, m=1):
-    return W*reduce(lambda x,y: x*y, [(1+r/m) for _ in xrange(n)], 1)
+    return W*reduce(lambda x,y: x*y, [(1+r/m) for _ in xrange(n*m)], 1)
 
-def plot_compound_interest(W,r,n):
+def plot_compound_interest(W,r,n, m=1):
     xaxis = xrange(n)
-    yaxis = [compound_interest(W,r,x) for x in xaxis]
+    yaxis = [compound_interest(W, r, x, m) for x in xaxis]
     pyplot.bar(xaxis,yaxis)
-    pyplot.xlabel("Years")
+    pyplot.xlabel("Time")
     pyplot.ylabel("Value")
     pyplot.title("Compound Interest with initial investment %s at rate %s" % (W,r))
     pyplot.savefig("Compound_Interest_%s" % (n))
