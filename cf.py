ac2b388edd2c6467498d8bc4261ec97f28ac8558
Mo Mirza <mo.mirza@iwoca.co.uk>
2013-01-26 15:27:05 +0000

    added docstrings

--- cf.py
+++ cf.py
@@ -1,12 +1,15 @@
 from matplotlib import pyplot
 
 def compound_interest(W, r, n, m=1):
+    """ compound interest calculator for given period and initial investment """
     return W*reduce(lambda x,y: x*y, [(1+r/m) for _ in xrange(n*m)], 1)
 
 def future_value(stream, r, m=1):
+    """ given a list of cashflows starting at t=0 return future value of stream at t=n """
     return sum([x*(1+r/m)**(len(stream)-k) for k, x in enumerate(stream)])
 
 def present_value(stream, r, m=1):
+    """ given a list of cashflows starting at t=0 return present value of stream """
     return sum([x/((1+r/m)**(k)) for k, x in enumerate(stream)])
 
 def plot_compound_interest(W,r,n, m=1):
