51b598d2cf1d7b49a503a267a17f33a1674d7d5a
Mo Mirza <mo.mirza@iwoca.co.uk>
2013-01-26 22:51:50 +0000

    added future rate calculation for spot rates

--- cf.py
+++ cf.py
@@ -8,9 +8,18 @@ def future_value(stream, r, m=1):
     """ given a list of cashflows starting at t=0 return future value of stream at t=n """
     return sum([x*(1+r/m)**(len(stream)-k) for k, x in enumerate(stream)])
 
-def present_value(stream, r, m=1):
+def present_value(stream, r, m=1, time=0):
     """ given a list of cashflows starting at t=0 return present value of stream """
-    return sum([x/((1+r/m)**(k)) for k, x in enumerate(stream)])
+    return sum([x/((1+r/m)**(k)) for k, x in enumerate(stream[time:])])
+
+def forward_rate(start, end, start_spot, end_spot):
+    """ NB not to be confused with a market forward rate """
+    return (((1.0+end_spot)**end)/(1.0+start_spot)**start)**(1.0/(end-start)) -1
+
+def future_rates(curve):
+    """ returns spot rate curve for next year under expectation dynamics """
+    start_rate = curve.pop(0)
+    return [forward_rate(1, n+2, start_rate, s) for n,s in enumerate(curve)]
 
 def plot_compound_interest(W,r,n, m=1):
     xaxis = xrange(n)
