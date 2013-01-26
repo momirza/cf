a5860c1dc3a79cbda0c9af475e6702aef63aed93
Mo Mirza <mo.mirza@iwoca.co.uk>
2013-01-26 15:24:32 +0000

    added future value and present value streams

--- cf.py
+++ cf.py
@@ -3,6 +3,12 @@ from matplotlib import pyplot
 def compound_interest(W, r, n, m=1):
     return W*reduce(lambda x,y: x*y, [(1+r/m) for _ in xrange(n*m)], 1)
 
+def future_value(stream, r, m=1):
+    return sum([x*(1+r/m)**(len(stream)-k) for k, x in enumerate(stream)])
+
+def present_value(stream, r, m=1):
+    return sum([x/((1+r/m)**(k)) for k, x in enumerate(stream)])
+
 def plot_compound_interest(W,r,n, m=1):
     xaxis = xrange(n)
     yaxis = [compound_interest(W, r, x, m) for x in xaxis]
